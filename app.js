c0ad0df391057f17fb779be004ef8971b91e25bf
Lyumina-n <nedokosl@gmail.com>
2023-05-25 21:14:51 +0300

    carousel element settings added

---
+++ app.js
@@ -0,0 +1,1 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-3AxznqoxKVWhlt+HsyNjjvD+X1j+vG0Sz4Cdo1J5Cn78vJPRtALg6A8nVJkC7Yvf" crossorigin="anonymous"></script>
